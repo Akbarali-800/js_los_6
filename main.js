8d1be2368a084e9ae9411d4b106f0520cd6be4a6
Akbarali-800 <mom1njonov2706@gmail.com>
2025-01-30 11:52:37 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,198 @@
+document.getElementById("priceButton").addEventListener("click", function () {
+    let quantity = +prompt("Nechta Buyurtirasiz?");
+    quantity = parseInt(quantity);
+
+    switch (true) {
+        case (quantity):
+            alert("Iltimos, to'g'ri son kiriting.");
+            break;
+        default:
+            let totalPrice = quantity * 45000;
+            alert("Jami narx: " + totalPrice + " so'm");
+            alert("Xaridingiz tayorlanmoqda")
+            break;
+    }
+});
+
+document.getElementById("priceButton2").addEventListener("click", function () {
+    let quantity = +prompt("Nechta Buyurtirasiz?");
+    quantity = parseInt(quantity);
+
+    switch (true) {
+        case (quantity):
+            alert("Iltimos, to'g'ri son kiriting.");
+            break;
+        default:
+            let totalPrice = quantity * 45000;
+            alert("Jami narx: " + totalPrice + " so'm");
+            alert("Xaridingiz tayorlanmoqda")
+            break;
+    }
+});
+
+
+document.getElementById("priceButton3").addEventListener("click", function () {
+    let quantity = +prompt("Nechta Buyurtirasiz?");
+    quantity = parseInt(quantity);
+
+    switch (true) {
+        case (quantity):
+            alert("Iltimos, to'g'ri son kiriting.");
+            break;
+        default:
+            let totalPrice = quantity * 45000;
+            alert("Jami narx: " + totalPrice + " so'm");
+            alert("Xaridingiz tayorlanmoqda")
+            break;
+    }
+});
+
+
+document.getElementById("priceButton4").addEventListener("click", function () {
+    let quantity = +prompt("Nechta Buyurtirasiz?");
+    quantity = parseInt(quantity);
+
+    switch (true) {
+        case (quantity):
+            alert("Iltimos, to'g'ri son kiriting.");
+            break;
+        default:
+            let totalPrice = quantity * 45000;
+            alert("Jami narx: " + totalPrice + " so'm");
+            alert("Xaridingiz tayorlanmoqda")
+            break;
+    }
+});
+
+
+document.getElementById("priceButton5").addEventListener("click", function () {
+    let quantity = +prompt("Nechta Buyurtirasiz?");
+    quantity = parseInt(quantity);
+
+    switch (true) {
+        case (quantity):
+            alert("Iltimos, to'g'ri son kiriting.");
+            break;
+        default:
+            let totalPrice = quantity * 45000;
+            alert("Jami narx: " + totalPrice + " so'm");
+            alert("Xaridingiz tayorlanmoqda")
+            break;
+    }
+});
+
+
+
+document.getElementById("priceButton6").addEventListener("click", function () {
+    let quantity = +prompt("Nechta Buyurtirasiz?");
+    quantity = parseInt(quantity);
+
+    switch (true) {
+        case (quantity):
+            alert("Iltimos, to'g'ri son kiriting.");
+            break;
+        default:
+            let totalPrice = quantity * 45000;
+            alert("Jami narx: " + totalPrice + " so'm");
+            alert("Xaridingiz tayorlanmoqda")
+            break;
+    }
+});
+
+
+document.getElementById("priceButton7").addEventListener("click", function () {
+    let quantity = +prompt("Nechta Buyurtirasiz?");
+    quantity = parseInt(quantity);
+
+    switch (true) {
+        case (quantity):
+            alert("Iltimos, to'g'ri son kiriting.");
+            break;
+        default:
+            let totalPrice = quantity * 45000;
+            alert("Jami narx: " + totalPrice + " so'm");
+            alert("Xaridingiz tayorlanmoqda")
+            break;
+    }
+});
+
+document.getElementById("priceButton8").addEventListener("click", function () {
+    let quantity = +prompt("Nechta Buyurtirasiz?");
+    quantity = parseInt(quantity);
+
+    switch (true) {
+        case (quantity):
+            alert("Iltimos, to'g'ri son kiriting.");
+            break;
+        default:
+            let totalPrice = quantity * 45000;
+            alert("Jami narx: " + totalPrice + " so'm");
+            alert("Xaridingiz tayorlanmoqda")
+            break;
+    }
+});
+
+document.getElementById("priceButton9").addEventListener("click", function () {
+    let quantity = +prompt("Nechta Buyurtirasiz?");
+    quantity = parseInt(quantity);
+
+    switch (true) {
+        case (quantity):
+            alert("Iltimos, to'g'ri son kiriting.");
+            break;
+        default:
+            let totalPrice = quantity * 45000;
+            alert("Jami narx: " + totalPrice + " so'm");
+            alert("Xaridingiz tayorlanmoqda")
+            break;
+    }
+});
+
+document.getElementById("priceButton10").addEventListener("click", function () {
+    let quantity = +prompt("Nechta Buyurtirasiz?");
+    quantity = parseInt(quantity);
+
+    switch (true) {
+        case (quantity):
+            alert("Iltimos, to'g'ri son kiriting.");
+            break;
+        default:
+            let totalPrice = quantity * 45000;
+            alert("Jami narx: " + totalPrice + " so'm");
+            alert("Xaridingiz tayorlanmoqda")
+            break;
+    }
+});
+
+document.getElementById("priceButton11").addEventListener("click", function () {
+    let quantity = +prompt("Nechta Buyurtirasiz?");
+    quantity = parseInt(quantity);
+
+    switch (true) {
+        case (quantity):
+            alert("Iltimos, to'g'ri son kiriting.");
+            break;
+        default:
+            let totalPrice = quantity * 45000;
+            alert("Jami narx: " + totalPrice + " so'm");
+            alert("Xaridingiz tayorlanmoqda")
+            break;
+    }
+});
+
+
+document.getElementById("priceButton12").addEventListener("click", function () {
+    let quantity = +prompt("Nechta Buyurtirasiz?");
+    quantity = parseInt(quantity);
+
+    switch (true) {
+        case (quantity):
+            alert("Iltimos, to'g'ri son kiriting.");
+            break;
+        default:
+            let totalPrice = quantity * 45000;
+            alert("Jami narx: " + totalPrice + " so'm");
+            alert("Xaridingiz tayorlanmoqda")
+            break;
+    }
+});
